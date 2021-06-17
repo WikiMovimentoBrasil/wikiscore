dc8b92a9118c051c029da414bfac24a03a1d7b73
albertoleoncio <40328018+albertoleoncio@users.noreply.github.com>
2021-06-17 10:05:05 -0300

    +Wiki Loves Bahia

--- bin/data.php
+++ bin/data.php
@@ -14,7 +14,33 @@ $contests_array = array(
 		'api_endpoint'			=> "https://pt.wikipedia.org/w/api.php",
 		'outreach_name'			=> 'Museu_Paulista/Wikiconcurso_Casa_Brasileira_(15_de_março_a_15_de_maio_de_2021)',
 		'theme'					=> 'amber'
+	),
+	'wlb_lista' => array(
+		'name_id' 				=> "wlb_lista",
+		'start_time' 			=> "1625875199",
+		'end_time' 				=> "1630454400",
+		'name' 					=> "Wikiconcurso Wiki Loves Bahia (Lista)",
+		'revert_time' 			=> "-24 hours",
+		'official_list_pageid' 	=> "6600495",
+		'category_pageid' 		=> "6610002",
+		'endpoint'				=> "https://pt.wikipedia.org/w/index.php",
+		'api_endpoint'			=> "https://pt.wikipedia.org/w/api.php",
+		'outreach_name'			=> 'Wiki_Movimento_Brasil/Wikiconcurso_Wiki_Loves_Bahia_(2021)',
+		'theme'					=> 'blue'
+	),
+	'wlb_geral' => array(
+		'name_id' 				=> "wlb_geral",
+		'start_time' 			=> "1625875199",
+		'end_time' 				=> "1630454400",
+		'name' 					=> "Wikiconcurso Wiki Loves Bahia (Geral)",
+		'revert_time' 			=> "-24 hours",
+		'endpoint'				=> "https://pt.wikipedia.org/w/index.php",
+		'api_endpoint'			=> "https://pt.wikipedia.org/w/api.php",
+		'outreach_name'			=> 'Wiki_Movimento_Brasil/Wikiconcurso_Wiki_Loves_Bahia_(2021)',
+		'theme'					=> 'blue-grey',
+		'category_petscan'		=> '19362327'
 	)
 );
 
 ?>
+ 
