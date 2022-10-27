0abd19118780f8aa89ec8909911a4ea6a7445221
Alberto Leoncio <albertoleoncio@hotmail.com>
2022-10-27 18:26:55 -0300

    protect.php: ajustes de código

--- bin/protect.php
+++ bin/protect.php
@@ -3,18 +3,19 @@
 // Logout, caso solicitado
 session_start();
 if (isset($_POST['logout'])) {
-	unset($_SESSION['user']);
+    unset($_SESSION['user']);
 }
 
-// Verifica se sessão pertence ao concurso em tela. Caso contrário, faz logout automaticamente.
+// Verifica se está logado e redireciona para login, caso necessário
 if (isset($_SESSION['user'])) {
-	if ($_SESSION['user']['contest'] != $contest['name_id']) {
-		unset($_SESSION['user']);
-	}
-}
 
-// Redireciona para login, caso necessário
-if (!isset($_SESSION['user'])) {
-	header("Location: index.php?contest=".$_GET['contest']."&page=login");
-	exit();
+    // Verifica se sessão pertence ao concurso em tela.
+    // Caso contrário, faz logout automaticamente.
+    if ($_SESSION['user']['contest'] != $contest['name_id']) {
+        unset($_SESSION['user']);
+    }
+
+} else {
+    header("Location: index.php?contest=".$_GET['contest']."&page=login");
+    exit();
 }
