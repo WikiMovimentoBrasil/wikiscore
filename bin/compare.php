d9e734a81bddf6f599d3cd89f06656d1403d72e6
Alberto Leoncio <albertoleoncio@hotmail.com>
2022-10-27 16:19:05 -0300

    compare.php: ajustes de código

--- bin/compare.php
+++ bin/compare.php
@@ -7,7 +7,7 @@ require_once "protect.php";
 require_once "data.php";
 
 //Verifica se a lista oficial e a categoria foram definidas
-if (isset($contest['official_list_pageid']) AND isset($contest['category_pageid'])) {
+if (isset($contest['official_list_pageid']) && isset($contest['category_pageid'])) {
 
     //Define arrays
     $list_cat = array();
@@ -25,42 +25,52 @@ if (isset($contest['official_list_pageid']) AND isset($contest['category_pageid'
 
     $list_api = json_decode(file_get_contents($contest['api_endpoint']."?".http_build_query($list_api_params)), true);
     $listmembers = end($list_api['query']['pages'])['links'];
-    foreach ($listmembers as $pagetitle) $list_official[] = $pagetitle['title'];
+    foreach ($listmembers as $pagetitle) {
+        $list_official[] = $pagetitle['title'];
+    }
 
     //Coleta segunda página da lista, caso exista
     while (isset($list_api['continue'])) {
         $list_api_params = [
             "action"        => "query",
-            "format"        => "json",
+            "format"        => "php",
             "prop"          => "links",
             "pageids"       => $contest['official_list_pageid'],
             "plnamespace"   => "0",
             "pllimit"       => "500",
             "plcontinue"    => $list_api['continue']['plcontinue']
         ];
-        $list_api = json_decode(file_get_contents($contest['api_endpoint']."?".http_build_query($list_api_params)), true);
+        $list_api = unserialize(
+            file_get_contents($contest['api_endpoint']."?".http_build_query($list_api_params))
+        );
         $listmembers = end($list_api['query']['pages'])['links'];
-        foreach ($listmembers as $pagetitle) $list_official[] = $pagetitle['title'];
+        foreach ($listmembers as $pagetitle) {
+            $list_official[] = $pagetitle['title'];
+        }
     }
 
     //Coleta lista de artigos na categoria
     $categorymembers_api_params = [
         "action"        => "query",
-        "format"        => "json",
+        "format"        => "php",
         "list"          => "categorymembers",
         "cmnamespace"   => "0",
         "cmpageid"      => $contest['category_pageid'],
         "cmprop"        => "title",
         "cmlimit"       => "500"
     ];
-    $categorymembers_api = json_decode(file_get_contents($contest['api_endpoint']."?".http_build_query($categorymembers_api_params)), true);
-    foreach ($categorymembers_api['query']['categorymembers'] as $pageid) $list_cat[] = $pageid['title'];
+    $categorymembers_api = unserialize(
+        file_get_contents($contest['api_endpoint']."?".http_build_query($categorymembers_api_params))
+    );
+    foreach ($categorymembers_api['query']['categorymembers'] as $pageid) {
+        $list_cat[] = $pageid['title'];
+    }
 
     //Coleta segunda página da categoria, caso exista
     while (isset($categorymembers_api['continue'])) {
         $categorymembers_api_params = [
             "action"        => "query",
-            "format"        => "json",
+            "format"        => "php",
             "list"          => "categorymembers",
             "cmnamespace"   => "0",
             "cmpageid"      => $contest['category_pageid'],
@@ -68,8 +78,12 @@ if (isset($contest['official_list_pageid']) AND isset($contest['category_pageid'
             "cmlimit"       => "500",
             "cmcontinue"    => $categorymembers_api['continue']['cmcontinue']
         ];
-        $categorymembers_api = json_decode(file_get_contents($contest['api_endpoint']."?".http_build_query($categorymembers_api_params)), true);
-        foreach ($categorymembers_api['query']['categorymembers'] as $pageid) $list_cat[] = $pageid['title'];
+        $categorymembers_api = unserialize(
+            file_get_contents($contest['api_endpoint']."?".http_build_query($categorymembers_api_params))
+        );
+        foreach ($categorymembers_api['query']['categorymembers'] as $pageid) {
+            $list_cat[] = $pageid['title'];
+        }
     }
 
     //Processa listagens
@@ -85,15 +99,46 @@ if (isset($contest['official_list_pageid']) AND isset($contest['category_pageid'
     $remover = array();
 }
 
-//Lista páginas pendentes de eliminação
-$ec_list = json_decode(file_get_contents($contest['api_endpoint']."?action=query&format=json&list=categorymembers&cmtitle=Categoria%3A!Itens%20propostos%20para%20elimina%C3%A7%C3%A3o&cmnamespace=4&cmlimit=500&cmprop=title"), true)['query']['categorymembers'];
-$esr_list = json_decode(file_get_contents($contest['api_endpoint']."?action=query&format=json&list=categorymembers&cmtitle=Categoria%3A!Todas_as_p%C3%A1ginas_para_elimina%C3%A7%C3%A3o_semirr%C3%A1pida&cmprop=title&cmnamespace=0&cmlimit=500"), true)['query']['categorymembers'];
-$er_list = json_decode(file_get_contents($contest['api_endpoint']."?action=query&format=json&list=categorymembers&cmtitle=Categoria%3A!P%C3%A1ginas%20para%20elimina%C3%A7%C3%A3o%20r%C3%A1pida&cmprop=title&cmnamespace=0&cmlimit=500"), true)['query']['categorymembers'];
-$caa_list = json_decode(file_get_contents($contest['api_endpoint']."?action=query&format=json&list=categorymembers&cmtitle=Categoria%3A!Artigos%20propostos%20para%20a%20candidatura&cmprop=title&cmnamespace=0&cmlimit=500"), true)['query']['categorymembers'];
-foreach ($ec_list as $page) $deletion[] = substr($page['title'], 34);
-foreach ($esr_list as $page) $deletion[] = $page['title'];
-foreach ($er_list as $page) $deletion[] = $page['title'];
-foreach ($caa_list as $page) $deletion[] = $page['title'];
+//Seleciona categoria de páginas pendentes de eliminação
+$ec_curid   = '1001045';
+$other_dels = [ '3501865' , '2419924' , '5857294' ];
+
+//Coleta páginas em EC
+$ec_list_params = [
+    "action"        => "query",
+    "format"        => "php",
+    "list"          => "categorymembers",
+    "cmpageid"      => $ec_curid,
+    "cmnamespace"   => "4",
+    "cmlimit"       => "max",
+    "cmprop"        => "title"
+];
+$ec_list = unserialize(
+    file_get_contents($contest['api_endpoint']."?".http_build_query($ec_list_params))
+)['query']['categorymembers'];
+foreach ($ec_list as $page) {
+    $deletion[] = substr($page['title'], 34);
+}
+
+//Coleta outras páginas
+foreach ($other_dels as $del_curid) {
+    $list_params = [
+        "action"        => "query",
+        "format"        => "php",
+        "list"          => "categorymembers",
+        "cmpageid"      => $del_curid,
+        "cmnamespace"   => "0",
+        "cmlimit"       => "max",
+        "cmprop"        => "title"
+    ];
+    $list = unserialize(
+        file_get_contents($contest['api_endpoint']."?".http_build_query($list_params))
+    )['query']['categorymembers'];
+    foreach ($list as $page) {
+        $deletion[] = $page['title'];
+    }
+}
+
 
 //Processa listagem
 $eliminar = array_intersect($deletion, $list_cat);
@@ -103,7 +148,8 @@ asort($eliminar);
 require_once "connect.php";
 
 //Coleta lista de diffs e artigos novos
-$inconsistency_query = mysqli_query($con,
+$inconsistency_query = mysqli_query(
+    $con,
     "SELECT
       `diff`,
       `valid_edit`,
@@ -119,8 +165,10 @@ $inconsistency_query = mysqli_query($con,
       )
       AND `valid_user` = '1'
       AND `reverted` IS NULL
-    ORDER BY `timestamp` ASC;");
-$wd_query = mysqli_query($con,
+    ORDER BY `timestamp` ASC;"
+);
+$wd_query = mysqli_query(
+    $con,
     "SELECT
       `article`
     FROM
@@ -129,7 +177,8 @@ $wd_query = mysqli_query($con,
     WHERE
       `{$contest['name_id']}__edits`.`new_page` = '1'
     ORDER BY
-      `{$contest['name_id']}__edits`.`timestamp` ASC;");
+      `{$contest['name_id']}__edits`.`timestamp` ASC;"
+);
 ?>
 
 <!DOCTYPE html>
@@ -152,7 +201,10 @@ $wd_query = mysqli_query($con,
                         <h3>Não listados</h3>
                     </div>
                     <ul class="w3-ul w3-border-top">
-                        <li>Os artigos abaixos estão inseridos na categoria, porém não estão inseridos na lista oficial</li>
+                        <li>
+                            Os artigos abaixos estão inseridos na categoria,
+                            porém não estão inseridos na lista oficial.
+                        </li>
                         <?php foreach ($adicionar as $artigo_add) {
                             $artigo_add_encoded = urlencode($artigo_add);
                             echo "<li>";
@@ -170,21 +222,26 @@ $wd_query = mysqli_query($con,
                         <h3>Descategorizados</h3>
                     </div>
                     <ul class="w3-ul w3-border-top">
-                        <li>Os artigos abaixos estão inseridos na lista oficial, porém não estão inseridos na categoria</li>
+                        <li>
+                            Os artigos abaixos estão inseridos na lista oficial,
+                            porém não estão inseridos na categoria.
+                        </li>
                         <?php foreach ($remover as $artigo_rem) {
                             $artigo_rem_params = [
                                 "action"        => "query",
-                                "format"        => "json",
+                                "format"        => "php",
                                 "prop"          => "links",
                                 "titles"        => $artigo_rem
                             ];
-                            $artigo_rem_api = json_decode(file_get_contents($contest['api_endpoint']."?".http_build_query($artigo_rem_params)), true)['query']['pages']['-1'];
-                            if (isset($artigo_rem_api)) continue;
+                            $artigo_rem_api = file_get_contents(
+                                $contest['api_endpoint']."?".http_build_query($artigo_rem_params)
+                            );
+                            $artigo_rem_api = unserialize($artigo_rem_api)['query']['pages']['-1'];
+                            if (isset($artigo_rem_api)) { continue; }
 
-                            $artigo_rem_encode = urlencode($artigo_rem);
                             echo "<li>";
                                 echo "<a target='_blank' href='{$contest['endpoint']}?title={$artigo_rem_encode}'>";
-                                    echo $artigo_rem;
+                                    echo urlencode($artigo_rem);
                                 echo "</a>";
                             echo "</li>\n";
                         }?>
@@ -197,7 +254,12 @@ $wd_query = mysqli_query($con,
                         <h3>Passíveis de eliminação</h3>
                     </div>
                     <ul class="w3-ul w3-border-top">
-                        <li>Os artigos abaixos estão inseridos na categoria e estão marcados em alguma modalidade de eliminação (rápida, semirrápida, por consenso ou por candidatura). Adicionalmente, também exibe artigos novos sem item no Wikidata.</li>
+                        <li>
+                            Os artigos abaixos estão inseridos na categoria e estão marcados em
+                            alguma modalidade de eliminação (rápida, semirrápida, por consenso
+                            ou por candidatura). Adicionalmente, também exibe artigos novos sem
+                            item no Wikidata.
+                        </li>
                         <?php foreach ($eliminar as $artigo_del) {
                             $artigo_del_encode = urlencode($artigo_del);
                             echo "<li>";
@@ -216,7 +278,7 @@ $wd_query = mysqli_query($con,
                             ];
                             $wd_api = file_get_contents($contest['api_endpoint']."?".http_build_query($wd_params));
                             $wd = end(json_decode($wd_api, true)["query"]["pages"]);
-                            if (isset($wd["pageprops"]["wikibase_item"])) continue;
+                            if (isset($wd["pageprops"]["wikibase_item"])) { continue; }
 
                             $wd_encode = urlencode($wd['pageid']);
                             echo "<li class='w3-green'>";
@@ -236,13 +298,17 @@ $wd_query = mysqli_query($con,
                         <h3>Inconsistências</h3>
                     </div>
                     <ul class="w3-ul w3-border-top">
-                        <li>As edições listadas abaixo pertecem a artigos que estavam listados na categoria mas foram removidos. Caso estejam marcadas de vermelho, a edição foi validada e conferiu pontos ao participante.</li>
+                        <li>
+                            As edições listadas abaixo pertecem a artigos que estavam listados
+                            na categoria mas foram removidos. Caso estejam marcadas de vermelho,
+                            a edição foi validada e conferiu pontos ao participante.
+                        </li>
                         <?php
                         while ($row = mysqli_fetch_assoc($inconsistency_query)) {
                             $diff_encode = urlencode($row['diff']);
 
                             echo "<li class='";
-                            if ($row['valid_edit'] == '1') echo "w3-red";
+                            if ($row['valid_edit'] == '1') { echo "w3-red"; }
                             echo "'>";
                                 echo "<a target='_blank' href='{$contest['endpoint']}?diff={$diff_encode}'>";
                                     echo $row['diff'];
