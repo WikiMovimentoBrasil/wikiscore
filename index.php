8fbe647fb33b215fa79551f9df440a29171066aa
albertoleoncio <albertoleoncio@hotmail.com>
2023-07-22 17:34:28 -0300

    index.php: +choose a language

--- index.php
+++ index.php
@@ -41,12 +41,12 @@ while ($row = mysqli_fetch_assoc($contests_result)) {
 }
 
 //Carrega traduções
-$acceptedLanguages = array_diff(scandir('translations'), array('..', '.'));
+$acceptedLanguages = str_replace('.json', '', array_diff(scandir('translations'), array('..', '.')));
 $userLang = filter_var($_GET["lang"] ?? "", FILTER_SANITIZE_STRING);
 $browserLang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) ?? '';
-if (in_array($userLang . '.json', $acceptedLanguages)) {
+if (in_array($userLang, $acceptedLanguages)) {
     $lang = $userLang;
-} elseif (in_array($browserLang . '.json', $acceptedLanguages)) {
+} elseif (in_array($browserLang, $acceptedLanguages)) {
     $lang = $browserLang;
 } else {
     $lang = 'en';
@@ -174,7 +174,7 @@ if (isset($_GET['contest'])) {
     ><?=§('contest-manage')?></button>
 </header>
 
-<!-- Modal -->
+<!-- Join -->
 <div id="id01" class="w3-modal">
     <div class="w3-modal-content w3-card-4 w3-animate-top">
         <header class="w3-container w3-deep-green">
@@ -191,6 +191,27 @@ if (isset($_GET['contest'])) {
         </div>
     </div>
 </div>
+
+<?php if (!$userLang): ?>
+    <?php require_once './bin/languages.php'; ?>
+    <!-- Language -->
+    <div id="id02" class="w3-modal" style="display: block;">
+        <div class="w3-modal-content w3-card-4 w3-animate-top">
+            <header class="w3-container w3-deep-green">
+                <span onclick="document.getElementById('id02').style.display='none'"
+                class="w3-button w3-display-topright">&times;</span>
+                <h4><?=§('language-select')?></h4>
+            </header>
+            <div class="w3-padding">
+            <?php foreach ($acceptedLanguages as $optionLanguage): ?>
+                <p>
+                    <a href='index.php?lang=<?=$optionLanguage?>'><?=$langCodes[$optionLanguage]?></a>
+                </p>
+            <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
 
 <!-- First Grid -->
 <div class="w3-row-padding w3-padding-64 w3-container">
