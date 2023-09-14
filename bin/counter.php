430bdae7569ddf3a75d72e4431c3110ffbc89a67
albertoleoncio <albertoleoncio@hotmail.com>
2023-09-14 18:46:56 -0300

    Merge branch 'feature/counter-superlative-stats' (resolve #55)

--- bin/counter.php
+++ bin/counter.php
@@ -183,27 +183,67 @@ if (mysqli_num_rows($count_query) == 0) { die("No users"); }
 $stats_query = mysqli_prepare(
     $con,
     "SELECT
-      SUM(`new_page`) AS `new_pages`,
-      COUNT(DISTINCT `article`) AS `edited_articles`,
-      SUM(`valid_edit`) AS `valid_edits`,
-      (
-        SELECT
-          SUM(`bytes`)
-        FROM
-          `{$contest['name_id']}__edits`
-        WHERE
-          `bytes` > 0
-      ) AS `all_bytes`,
-      (
-        SELECT
-          COUNT(*)
-        FROM
-          `{$contest['name_id']}__users`
-        WHERE
-          `timestamp` != '0'
-      ) AS `all_users`
+        most_edited.title AS most_edited_title,
+        most_edited.article AS most_edited_article,
+        most_edited.total AS most_edited_total,
+        biggest_delta.title AS biggest_delta_title,
+        biggest_delta.article AS biggest_delta_article,
+        biggest_delta.total AS biggest_delta_total,
+        biggest_edition.title AS biggest_edition_title,
+        biggest_edition.article AS biggest_edition_article,
+        biggest_edition.diff AS biggest_edition_diff,
+        biggest_edition.bytes AS biggest_edition_bytes,
+        edits_summary.new_pages AS new_pages,
+        edits_summary.edited_articles AS edited_articles,
+        edits_summary.valid_edits AS valid_edits,
+        edits_summary.all_bytes AS all_bytes,
+        edits_summary.all_users AS all_users
     FROM
-      `{$contest['name_id']}__edits`"
+        (SELECT title, article, COUNT(diff) AS total, SUM(bytes) AS bytes
+        FROM {$contest['name_id']}__edits
+        LEFT JOIN {$contest['name_id']}__articles ON article = articleID
+        GROUP BY article
+        ORDER BY total DESC
+        LIMIT 1) AS most_edited
+    LEFT JOIN
+        (SELECT title, article, SUM(bytes) AS total
+        FROM {$contest['name_id']}__edits
+        LEFT JOIN {$contest['name_id']}__articles ON article = articleID
+        GROUP BY article
+        ORDER BY total DESC
+        LIMIT 1) AS biggest_delta
+    ON 1=1
+    LEFT JOIN
+        (SELECT title, article, diff, bytes
+        FROM {$contest['name_id']}__edits
+        LEFT JOIN {$contest['name_id']}__articles ON article = articleID
+        WHERE valid_edit = '1'
+        ORDER BY bytes DESC
+        LIMIT 1) AS biggest_edition
+    ON 1=1
+    CROSS JOIN
+        (SELECT
+            SUM(`new_page`) AS `new_pages`,
+            COUNT(DISTINCT `article`) AS `edited_articles`,
+            SUM(`valid_edit`) AS `valid_edits`,
+            (
+                SELECT
+                    SUM(`bytes`)
+                FROM
+                    `{$contest['name_id']}__edits`
+                WHERE
+                    `bytes` > 0
+            ) AS `all_bytes`,
+            (
+                SELECT
+                    COUNT(*)
+                FROM
+                    `{$contest['name_id']}__users`
+                WHERE
+                    `timestamp` != '0'
+            ) AS `all_users`
+        FROM
+            `{$contest['name_id']}__edits`) AS edits_summary;"
 );
 mysqli_stmt_execute($stats_query);
 $stats = mysqli_fetch_assoc(mysqli_stmt_get_result($stats_query));
@@ -305,6 +345,58 @@ while (isset($list_api['continue'])) {
                     <div class="w3-third">
                         <h6 class="w3-center"><?=§('counter-allbytes')?></h6>
                         <h1 class="w3-center"><?=number_format($stats['all_bytes'], 0, ',', '.');?></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="w3-row-padding w3-center w3-section">
+                <div class="w3-third">
+                    <div class="w3-container w3-card w3-<?=$contest['theme'];?>">
+                        <div class="w3-half">
+                            <h6 class="w3-center"><?=§('counter-most-edited')?></h6>
+                        </div>
+                        <div class="w3-half">
+                            <h4 class="w3-center">
+                                <a href="<?=$contest['endpoint']?>?curid=<?=$stats['most_edited_article']?>">
+                                    <?=$stats['most_edited_title']??§('edits-title')?>
+                                </a>
+                                <br>
+                                <?=§('counter-editions', number_format($stats['most_edited_total'], 0, ',', '.'))?>
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="w3-third">
+                    <div class="w3-container w3-card w3-<?=$contest['theme'];?>">
+                        <div class="w3-half">
+                            <h6 class="w3-center"><?=§('counter-biggest-delta')?></h6>
+                        </div>
+                        <div class="w3-half">
+                            <h4 class="w3-center">
+                                <a href="<?=$contest['endpoint']?>?curid=<?=$stats['biggest_delta_article']?>">
+                                    <?=$stats['biggest_delta_title']??§('edits-title')?>
+                                </a>
+                                <br>
+                                <?=§('triage-bytes', number_format($stats['biggest_delta_total'], 0, ',', '.'))?>
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="w3-third">
+                    <div class="w3-container w3-card w3-<?=$contest['theme'];?>">
+                        <div class="w3-half">
+                            <h6 class="w3-center"><?=§('counter-biggest-edition')?></h6>
+                        </div>
+                        <div class="w3-half">
+                            <h4 class="w3-center">
+                                <a href="<?=$contest['endpoint']?>?diff=<?=$stats['biggest_edition_diff']?>">
+                                    <?=$stats['biggest_edition_title']??§('edits-title')?>
+                                </a>
+                                <br>
+                                <?=§('triage-bytes', number_format($stats['biggest_edition_bytes'], 0, ',', '.'))?>
+                            </h4>
+                        </div>
                     </div>
                 </div>
             </div>
