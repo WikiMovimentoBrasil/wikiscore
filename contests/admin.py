7df964a4b92fe251928caf040619b911451ff158
albertoleoncio <albertoleoncio@hotmail.com>
2024-08-23 11:32:18 -0300

    chore: Add ParticipantEnrollment model to admin site

--- contests/admin.py
+++ contests/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from contests.models import Contest, Group, Article, Participant, Edit, Qualification, Evaluation, Evaluator
+from contests.models import Contest, Group, Article, Participant, Edit, Qualification, Evaluation, Evaluator, ParticipantEnrollment
 
 # Register your models here.
 admin.site.register(Contest)
@@ -10,3 +10,4 @@ admin.site.register(Edit)
 admin.site.register(Qualification)
 admin.site.register(Evaluation)
 admin.site.register(Evaluator)
+admin.site.register(ParticipantEnrollment)
