6221faaa6ea655e0aff06547055cde55cbd617d9
albertoleoncio <albertoleoncio@hotmail.com>
2024-08-26 12:46:28 -0300

    chore: Add counter view and template for contests

--- contests/urls.py
+++ contests/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import contest_view, home_view, color_view, triage_view
+from .counter import counter_view
 
 urlpatterns = [
     path('', home_view, name='home_view'),
     path('contests/', contest_view, name='contest_view'),
     path('color/', color_view, name='color_view'),
     path('triage/', triage_view, name='triage_view'),
+    path('counter/', counter_view, name='counter_view'),
 ]
