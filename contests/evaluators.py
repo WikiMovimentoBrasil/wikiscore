d2f7e3ee5397ed7392d0c62d76a6d3d1711c324d
albertoleoncio <albertoleoncio@hotmail.com>
2024-08-29 13:08:01 -0300

    chore: Refactor evaluators handler to improve code structure and readability

--- contests/evaluators.py
+++ contests/evaluators.py
@@ -9,79 +9,89 @@ class EvaluatorsHandler:
         self.contest = contest
 
     def execute(self, request):
-        contest = self.contest
-        if request.method == 'POST' and Evaluator.objects.get(contest=contest, profile=request.user.profile).user_status == 'G':
-            if request.POST.get('new'):
-                username = request.POST.get('new')
-                try:
-                    profile = Profile.objects.get(username=username)
-                except Profile.DoesNotExist:
-                    api_params = {
-                        'action': 'query',
-                        'meta': 'globaluserinfo',
-                        'guiuser': username,
-                        'format': 'json',
-                    }
-                    response = requests.get(contest.api_endpoint, params=api_params)
-                    data = response.json()
+        evaluator = self.get_current_evaluator(request)
+        
+        if request.method == 'POST':
+            if evaluator.user_status == 'G':
+                if request.POST.get('new'):
+                    self.add_new_evaluator(request.POST.get('new'))
 
-                    if 'query' in data and 'globaluserinfo' in data['query'] and 'id' in data['query']['globaluserinfo']:
-                        global_id = data['query']['globaluserinfo']['id']
-                        profile = Profile.objects.create(global_id=global_id, username=username)
-                        Evaluator.objects.create(contest=contest, profile=profile, user_status='A')
+                if request.POST.get('user'):
+                    self.update_evaluator_status(request)
 
-                else:
-                    Evaluator.objects.create(contest=contest, profile=profile, user_status='A')
-                
-            if request.POST.get('user'):
-                if request.POST.get('off'):
-                    Evaluator.objects.filter(
-                        contest=contest,
-                        profile__username=request.POST.get('user'),
-                    ).update(user_status='P')
-
-                if request.POST.get('on'):
-                    Evaluator.objects.filter(
-                        contest=contest,
-                        profile__username=request.POST.get('user'),
-                    ).update(user_status='A')
-
-                if request.POST.get('reset'):
-                    Edit.objects.filter(
-                        contest=contest,
-                        last_evaluation__evaluator__profile__username=request.POST.get('user'),
-                    ).update(last_evaluation=None)
-
-        evaluators = Profile.objects.filter(
-                evaluator__contest=contest,
-                evaluator__user_status='A',
-            ).annotate(
-                evaluation_count=Count('evaluator__evaluation'),
-                filter=Q(evaluator__evaluation__status='1')
-            ).values('global_id', 'username', 'evaluation_count')
-        managers = Profile.objects.filter(
-                evaluator__contest=contest,
-                evaluator__user_status='G',
-            ).annotate(
-                evaluation_count=Count('evaluator__evaluation'),
-                filter=Q(evaluator__evaluation__status='1')
-            ).values('global_id', 'username', 'evaluation_count')
-        disabled = Profile.objects.filter(
-                Q(evaluator__contest=contest, evaluator__user_status='P') |
-                ~Q(evaluator__contest=contest)
-            ).annotate(
-                evaluation_count=Count('evaluator__evaluation'),
-                filter=Q(evaluator__evaluation__status='1')
-            ).values('global_id', 'username', 'evaluation_count')
-
-        return_dict = {
-            'contest': contest,
-            'evaluators': evaluators,
-            'managers': managers,
-            'disabled': disabled,
-            'status': Evaluator.objects.get(contest=contest, profile=request.user.profile).user_status,
+        return {
+            'contest': self.contest,
+            'evaluators': self.get_evaluators_by_status('A'),
+            'managers': self.get_evaluators_by_status('G'),
+            'disabled': self.get_disabled_evaluators(),
+            'status': evaluator.user_status,
             'right': 'left' if translation.get_language_bidi() else 'right',
             'left': 'right' if translation.get_language_bidi() else 'left',
         }
 
-        return return_dict
+    def get_current_evaluator(self, request):
+        return Evaluator.objects.get(contest=self.contest, profile=request.user.profile)
+
+    def add_new_evaluator(self, username):
+        profile = self.get_or_create_profile(username)
+        if profile:
+            Evaluator.objects.create(contest=self.contest, profile=profile, user_status='A')
+
+    def get_or_create_profile(self, username):
+        try:
+            return Profile.objects.get(username=username)
+        except Profile.DoesNotExist:
+            return self.fetch_profile_from_api(username)
+
+    def fetch_profile_from_api(self, username):
+        api_params = {
+            'action': 'query',
+            'meta': 'globaluserinfo',
+            'guiuser': username,
+            'format': 'json',
+        }
+        response = requests.get(self.contest.api_endpoint, params=api_params)
+        data = response.json()
+
+        if 'query' in data and 'globaluserinfo' in data['query'] and 'id' in data['query']['globaluserinfo']:
+            global_id = data['query']['globaluserinfo']['id']
+            return Profile.objects.create(global_id=global_id, username=username)
+
+    def update_evaluator_status(self, request):
+        username = request.POST.get('user')
+        if request.POST.get('off'):
+            self.update_status(username, 'P')
+        elif request.POST.get('on'):
+            self.update_status(username, 'A')
+        elif request.POST.get('reset'):
+            self.reset_evaluations(username)
+
+    def update_status(self, username, status):
+        Evaluator.objects.filter(
+            contest=self.contest,
+            profile__username=username,
+        ).update(user_status=status)
+
+    def reset_evaluations(self, username):
+        Edit.objects.filter(
+            contest=self.contest,
+            last_evaluation__evaluator__profile__username=username,
+        ).update(last_evaluation=None)
+
+    def get_evaluators_by_status(self, status):
+        return Profile.objects.filter(
+            evaluator__contest=self.contest,
+            evaluator__user_status=status,
+        ).annotate(
+            evaluation_count=Count('evaluator__evaluation'),
+            filter=Q(evaluator__evaluation__status='1')
+        ).values('global_id', 'username', 'evaluation_count')
+
+    def get_disabled_evaluators(self):
+        return Profile.objects.filter(
+            Q(evaluator__contest=self.contest, evaluator__user_status='P') |
+            ~Q(evaluator__contest=self.contest)
+        ).annotate(
+            evaluation_count=Count('evaluator__evaluation'),
+            filter=Q(evaluator__evaluation__status='1')
+        ).values('global_id', 'username', 'evaluation_count')
