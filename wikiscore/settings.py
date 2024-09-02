989e060f0c8e4d19b0a57f82ccb224907b0f1e18
albertoleoncio <albertoleoncio@hotmail.com>
2024-09-01 21:43:31 -0300

    chore: Update STATIC_ROOT in settings.py for static file serving

--- wikiscore/settings.py
+++ wikiscore/settings.py
@@ -134,7 +134,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(HOME, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
