af8ec2d33e8798e265ba3c5aea4d6c75b618b074
albertoleoncio <albertoleoncio@hotmail.com>
2024-09-03 14:08:39 -0300

    chore: Update static file paths in .gitignore and settings.py

--- wikiscore/settings.py
+++ wikiscore/settings.py
@@ -133,8 +133,8 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(HOME, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- wikiscore/urls.py
+++ wikiscore/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('', include('credentials.urls')),
     path('', include('social_django.urls')),
     path("i18n/", include("django.conf.urls.i18n")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
