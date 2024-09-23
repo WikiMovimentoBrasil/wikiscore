efbffaae65458c98e4f9009f9d0aa2a3918affec
albertoleoncio <albertoleoncio@hotmail.com>
2024-09-23 16:32:40 -0300

    refactor: Refactor translate command to use polib for loading and saving .po and .mo files

--- contests/management/commands/translate.py
+++ contests/management/commands/translate.py
@@ -21,6 +21,16 @@ class Command(BaseCommand):
                 call_command('makemessages', f'-l{language_code}')
                 po_path = os.path.join(settings.BASE_DIR, 'locale', language_code, 'LC_MESSAGES', 'django.po')
                 po = self.convert_to_po(translations, language_code, po_path)
+                if language_code == 'en':
+                    po = polib.pofile(po_path, encoding='utf-8')
+                    self.synchronize_po_with_json(po, translations, filepath)
+
+                # Compile the messages
+                mo = polib.MOFile()
+                mo.metadata = po.metadata
+                for entry in po:
+                    mo.append(entry)
+                mo.save(po_path.replace('.po', '.mo'))
 
     def load_translations(self, filepath):
         with open(filepath, 'r', encoding='utf-8') as f:
@@ -52,8 +62,52 @@ class Command(BaseCommand):
                 if entry:
                     entry.msgstr = value
 
+        # Check for missing translations
+        for entry in po:
+            if entry.msgstr == '':
+                entry.msgstr = entry.msgid
+
         po.save(po_path)
         print(f"Successfully converted to {po_path}")
+        return po
+
+    def synchronize_po_with_json(self, po, translations, json_path):
+        # Flatten JSON for easy lookup
+        flat_json = self.flatten_dict(translations)
+
+        # 1. Add new PO entries to the JSON file
+        for entry in po:
+            if entry.msgctxt not in flat_json:
+                # New message found, add it to the JSON
+                if entry.msgctxt is not None:
+                    flat_json[entry.msgctxt] = entry.msgid
+                    print(f"New message added to JSON: {entry.msgctxt}")
+                else:
+                    raise ValueError(f"Message without context in PO file: {entry.msgid}")
+
+        # 2. Remove entries from the JSON file if not present in the PO file
+        json_keys_to_remove = []
+        for key in flat_json:
+            if not any(entry.msgctxt == key for entry in po):
+                # Message not found in PO, mark it for removal from JSON
+                json_keys_to_remove.append(key)
+
+        for key in json_keys_to_remove:
+            del flat_json[key]
+            print(f"Message removed from JSON: {key}")
+
+        # 3. Check for discrepancies between JSON and PO messages
+        for entry in po:
+            msgid_sub = re.sub(r'\%\((\d+)\)s', '$\\1', entry.msgid)
+            if entry.msgctxt in flat_json and flat_json[entry.msgctxt] != msgid_sub:
+                print(f"Warning: Different message in PO and JSON for context '{entry.msgctxt}': PO='{msgid_sub}' JSON='{flat_json[entry.msgctxt]}'")
+
+        # 4. Save the updated JSON file
+        updated_json = self.unflatten_dict(flat_json)
+        with open(json_path, 'w', encoding='utf-8') as f:
+            json.dump(updated_json, f, ensure_ascii=False, indent=4)
+        print(f"Successfully synchronized {json_path} with the PO file.")
+        return po
 
     def flatten_dict(self, d, parent_key='', sep='.'):
         """Flattens a nested dictionary."""
@@ -65,6 +119,17 @@ class Command(BaseCommand):
             else:
                 items.append((new_key, v))
         return dict(items)
+
+    def unflatten_dict(self, flat_dict, sep='.'):
+        """Unflattens a dictionary."""
+        unflattened = {}
+        for key, value in flat_dict.items():
+            keys = key.split(sep)
+            d = unflattened
+            for k in keys[:-1]:
+                d = d.setdefault(k, {})
+            d[keys[-1]] = value
+        return unflattened
     
 
     
