15f68fe2814704e2dcf8b77cceb00aee6f5438cd
albertoleoncio <albertoleoncio@hotmail.com>
2024-10-17 16:12:37 -0300

    feat: Add whitenoise middleware

--- wikiscore/settings.py
+++ wikiscore/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
