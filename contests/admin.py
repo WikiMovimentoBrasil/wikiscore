d19d4cae2faba6ae942f39975be535375b40f50b
albertoleoncio <albertoleoncio@hotmail.com>
2026-09-09 10:51:06 -0300

    refactor: Organize admin model registrations for better readability

--- contests/admin.py
+++ contests/admin.py
@@ -17,12 +17,33 @@ from contests.models import (
 admin.site.register(Contest)
 admin.site.register(Group)
 admin.site.register(Article)
-admin.site.register(Participant)
-admin.site.register(EditWikidata)
-admin.site.register(Qualification)
-admin.site.register(Evaluation)
 admin.site.register(Evaluator)
-admin.site.register(ParticipantEnrollment)
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    raw_id_fields = ('contest', 'last_enrollment')
+
+
+@admin.register(ParticipantEnrollment)
+class ParticipantEnrollmentAdmin(admin.ModelAdmin):
+    raw_id_fields = ('contest', 'user')
+
+
+@admin.register(Qualification)
+class QualificationAdmin(admin.ModelAdmin):
+    raw_id_fields = ('contest', 'diff', 'evaluator')
+
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    raw_id_fields = ('contest', 'diff', 'evaluator')
+
+
+@admin.register(EditWikidata)
+class EditWikidataAdmin(admin.ModelAdmin):
+    raw_id_fields = ('edit',)
+
 
 @admin.register(Edit)
 class EditAdmin(admin.ModelAdmin):
